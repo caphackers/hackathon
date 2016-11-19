891ec24eba248c15d16704edc967808080874916
Massinissa Ikhelef <massinissa.ikhelef@capgemini.com>
2016-11-19 02:29:46 +0100

    dfsfds

--- app/js/app.js
+++ app/js/app.js
@@ -2,11 +2,11 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version',
-  'todoList'
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version',
+    'todoList'
 ]).config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
 
 }]);
@@ -15,51 +15,86 @@ angular.module('myApp', [
 var todoList = angular.module('todoList', []);
 
 todoList.controller('todoCtrl', ['$scope', '$http',
-  function ($scope, $http) {
+    function ($scope, $http) {
 
-    var urls = $scope.urls = [];
+        var urls = $scope.urls = [];
 
-    $scope.addUrl = function () {
-      var newUrl = $scope.newUrl.trim();
-      var url = "https://watson-api-explorer.mybluemix.net/visual-recognition/api/v3/detect_faces?api_key=2bf3d9e76fed69e0c6309b47bc40760bb8936da3&url="
-          + newUrl + "&version=2016-05-20";
-      $http({
-        method: 'POST',
-        url: url,
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      }).then(function successCallback(response) {
-        console.log(response.data.images[0].faces[0]);
-        $scope.facedetection = response
-      }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
-      if (!newUrl.length) {
-        return;
-      }
-      urls.push({
-        title: newUrl,
-        completed: false
-      });
-      $scope.newUrl = '';
-    };
+        $scope.addUrl = function () {
 
-    $scope.removeUrls = function (todo) {
-      urls.splice(urls.indexOf(todo), 1);
-    };
-    $scope.markAll = function (completed) {
-      urls.forEach(function (todo) {
-        todo.completed = !completed;
-      });
-    };
+            var newUrl = $scope.newUrl;
 
-    $scope.clearCompletedUrls = function () {
-      $scope.urls = urls = urls.filter(function (todo) {
-        return !todo.completed;
-      });
-    };
-  }
+            var apiWatsonKey = "2bf3d9e76fed69e0c6309b47bc40760bb8936da3";
+            var collectionUrl = "https://watson-api-explorer.mybluemix.net/visual-recognition/api/v3/collections?api_key=2bf3d9e76fed69e0c6309b47bc40760bb8936da3&name=name&version=2016-05-20";
+
+            $http({
+                method: 'GET',
+                url: collectionUrl,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function successCallback(response) {
+                if (response.data.collections.length > 0) {
+                    console.log(response.data.collections);
+                } else {
+                    var collectionName =  Math.random().toString(36).substring(7);
+
+                    var form = new FormData();
+                    form.append("name=name", "");
+
+
+                    $http({
+                        method: 'POST',
+                        url: collectionUrl,
+                        headers: {
+                            'Accept': 'application/json',
+                        },
+                        "data": form
+                    }).then(function successCallback(response) {
+                    }, function errorCallback(response) {
+                    });
+                }
+            }, function errorCallback(response) {
+            });
+
+
+            var detectFacesUrl = "https://watson-api-explorer.mybluemix.net/visual-recognition/api/v3/detect_faces?api_key="
+                + apiWatsonKey + "&url="
+                + newUrl + "&version=2016-05-20";
+            $http({
+                method: 'POST',
+                url: detectFacesUrl,
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function successCallback(response) {
+                console.log(response.data.images[0].faces[0]);
+            }, function errorCallback(response) {
+            });
+            if (!newUrl.length) {
+                return;
+            }
+            urls.push({
+                title: newUrl,
+                completed: false
+            });
+            $scope.newUrl = '';
+        };
+
+        $scope.removeUrls = function (todo) {
+            urls.splice(urls.indexOf(todo), 1);
+        };
+        $scope.markAll = function (completed) {
+            urls.forEach(function (todo) {
+                todo.completed = !completed;
+            });
+        };
+
+        $scope.clearCompletedUrls = function () {
+            $scope.urls = urls = urls.filter(function (todo) {
+                return !todo.completed;
+            });
+        };
+    }
 ]);
